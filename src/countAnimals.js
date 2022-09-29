3788c0c74d61b61b58f60c369ba21fdde40ed805
Gabriela Muniz <gabrielanamu@live.com>
2022-09-29 17:11:10 -0300

    adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal = null) {
+  if (animal === null) {
+    return data.species.reduce((acc, specie) => {
+      const nome = specie.name;
+      acc[nome] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  if (Object.keys(animal).includes('sex')) {
+    const name = data.species.find((specie) => specie.name === animal.specie).residents;
+    return name.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return data.species.find((specie) => specie.name === animal.specie).residents.length;
 }
+console.log(countAnimals());
 
 module.exports = countAnimals;
