e6011ee61d0478389e5099545d3315c06ecf0ed1
Gabriela Muniz <gabrielanamu@live.com>
2022-09-30 16:22:30 -0300

    adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const pessoa = data.employees.find((employee) => employee.id === id);
+  const primeiroAnimal = data.species.find((specie) => specie.id === pessoa.responsibleFor[0]);
+  const reduceAnimal = primeiroAnimal.residents.reduce((acc, resident) => {
+    if (acc.age < resident.age) {
+      return resident;
+    }
+    return acc;
+  });
+  return Object.values(reduceAnimal);
 }
+console.log(getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = getOldestFromFirstSpecies;
