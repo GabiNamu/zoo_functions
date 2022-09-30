0a482a6776e01e691c83a8bd8030b6ff6a1ff1b2
Gabriela Muniz <gabrielanamu@live.com>
2022-09-30 11:34:58 -0300

    adiciona requisito 16

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,23 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants = []) {
-  // if (Array.isArray(entrants)) {
-    return entrants.reduce((acc, entrant) => {
-      if (entrant.age < 18) {
-        acc.child += 1;
-      }
-      if (entrant.age >= 18 && entrant.age < 50) {
-        acc.adult += 1;
-      }
-      if (entrant.age >= 50) {
-        acc.senior += 1;
-      }
-      return acc;
-    }, { child: 0, adult: 0, senior: 0 });
-//   }
-//  return 0;
+  return entrants.reduce((acc, entrant) => {
+    if (entrant.age < 18) {
+      acc.child += 1;
+    }
+    if (entrant.age >= 18 && entrant.age < 50) {
+      acc.adult += 1;
+    }
+    if (entrant.age >= 50) {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
-// console.log(countEntrants({}));
 
 function calculateEntry(entrants = []) {
   if (entrants === []) {
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -33,4 +33,10 @@ describe('Testes da função getOpeningHours', () => {
   test('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: "The minutes should represent a number"', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError('The minutes should represent a number');
   });
+  test('Para os argumentos Monday e 13:00-AM deve lançar uma exceção com a mensagem: "The hour must be between 0 and 12"', () => {
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrowError('The hour must be between 0 and 12');
+  });
+  test('Para os argumentos Tuesday e 09:60-AM deve lançar uma exceção com a mensagem: "The minutes must be between 0 and 59"', () => {
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrowError('The minutes must be between 0 and 59');
+  });
 });
