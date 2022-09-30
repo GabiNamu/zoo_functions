2aede41071504c45a1bcd09791cb92cef83ce31c
Gabriela Muniz <gabrielanamu@live.com>
2022-09-30 20:01:48 -0300

    adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const employee = data.employees.find((employe) => employe.id === id).firstName;
+  // console.log(employee);
+  if (employee === 'Burl' || employee === 'Ola' || employee === 'Stephanie') {
+    return true;
+  }
+  if (employee === 'Emery') {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const relatedEmployessFilter = data.employees.filter((employee) => {
+    const relatedEmployees = employee.managers.find((element) => element === managerId);
+    return relatedEmployees;
+  });
+  return relatedEmployessFilter.map((element) => `${element.firstName} ${element.lastName}`);
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
